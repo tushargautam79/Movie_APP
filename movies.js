de1b7dd469a80d0def5048b298cd5071df59ad98
tushargautam79 <43692496+tushargautam79@users.noreply.github.com>
2021-07-24 23:34:19 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -7,14 +7,18 @@ var LocalStrategy= require("passport-local");
 var passportLocalMongoose=require("passport-local-mongoose");
 var request=require("request");
 var bodyParser=require("body-parser");
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://Gautam79:Gautam79@cluster0.hswd9.mongodb.net/mysecondDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.Promise = global.Promise;
+try {
+     mongoose.connect(
+      'mongodb+srv://Gautam79:Gautam79@cluster0.hswd9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Database is connected")
+    );
+
+  } catch (e) {
+    console.log("could not connect database");
+}
 var userSchema= new mongoose.Schema({
 	email: {type:String ,required:true, unique:true},
 	username: {type:String ,required:true, unique:true},
